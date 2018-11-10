dc3f03f60fd747490b7f0481cb31bc05647bf4f8
Suva Shahria <ss2479@null.cs.rutgers.edu>
2018-11-10 13:08:36 -0500

    bubble parallel ver done writeup tomorrow

--- problem_1/bubble.c
+++ problem_1/bubble.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <assert.h>
 
 void print(int arr[], int size);
-void sort(int arr[], int size) ;
+void sort(int arr[], int size);
+void* sort_pthread(void *param);
 
+int num_threads;
+
+pthread_t * tids;
+pthread_mutex_t * mutexes;
+
+int* Array;
+int* Arr2;
+long size = 10000000;
 
 int main(void) {
 
-	srand(time(NULL));
 
-	int Array[25];
-
-	int size = sizeof(Array)/sizeof(Array[0]);
-
-Array[0] = rand() % 20;
+clock_t start, stop;
 
 
+
+Array = (int*) malloc(size*sizeof(int));
+
+
+srand(time(NULL));
 
 	int i;
 	for(i=0; i < size; i++){
@@ -24,14 +36,114 @@ Array[0] = rand() % 20;
 	}
 
 
-	print(Array,size);
-	sort(Array,size);
-	printf("\n");
-	print(Array,size);
-	printf("\n");
-	return 0;
+   double t;
+
+  assert((start=clock())!=-1);
+  	sort(Array,size);
+   stop = clock();
+   t = (double) (stop-start)/(CLOCKS_PER_SEC);
+
+
+
+   printf("Sequential Time: %g\n", t/100);
+
+	//print(Array,size);
+
+	//printf("\n");
+   //thread imple
+
+
+
+Arr2 = (int*) malloc(size*sizeof(int));
+
+	i = 0;
+	for(i=0;i<size;i++){
+		Arr2[i] = Array[i];
+
+	}
+
+
+	num_threads = 4;
+
+	   tids = malloc(num_threads *sizeof(pthread_t));
+	   mutexes = malloc(num_threads *sizeof(pthread_mutex_t));
+	   i = 0;
+	   for(i=0;i<num_threads;i++){
+		   pthread_mutex_init(&mutexes[i],NULL);
+	   }
+
+
+
+
+
+	       for(i=0;i<num_threads;i++){
+
+	           pthread_create(&tids[i],NULL,sort_pthread,(void*)0);
+	       }
+	       for(i=0; i<num_threads;i++){
+	    	   pthread_join(tids[i],NULL);
+	       }
+
+	  // 	print(Arr2,size);
+
+	 //  	printf("\n");
+
+	       for( i = 0; i < size; i++){
+	    	   if(Arr2[i] != Array[i]){
+	    		   printf("%d  error\n",i);
+	    	   }
+
+	       }
+
+return 0;
 }
 
+
+void* sort_pthread(void *param) {
+	 int i=0,k=0, tmp=0, start=0, end=0;
+
+
+	  int  per_thread = size/num_threads;
+
+	  for(i = 0; i<per_thread; i++){
+		  for(k=0;k<num_threads;k++){
+			  start = per_thread * k;
+			  end = start + per_thread;
+
+			  if(k == (num_threads-1) ){
+				  end = size;
+			  }
+			  if(k==0) pthread_mutex_lock(&mutexes[0]);
+
+	            for(i=start;i<end-1;i++){
+	                if(Arr2[i]>Arr2[i+1]){
+	                    tmp=Arr2[i];
+	                    Arr2[i]=Arr2[i+1];
+	                    Arr2[i+1]=tmp;
+	                }
+	            }
+
+	            if(k==num_threads-1){
+	                pthread_mutex_unlock(&mutexes[k]);
+	                break;
+	            }
+/*
+	            pthread_mutex_lock(&mutexes[k+1]);
+                if(Arr2[i]>Arr2[i+1]){
+                    tmp=Arr2[i];
+                    Arr2[i]=Arr2[i+1];
+                    Arr2[i+1]=tmp;
+                }
+
+                */
+	            pthread_mutex_unlock(&mutexes[k]);
+
+		  }
+	  }
+
+
+	return NULL;
+}
 
 
 void sort(int arr[], int size)
