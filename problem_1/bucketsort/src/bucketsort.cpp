fc5584105ae6c241387918c0e0da32e91659430f
Ryan Morey <ryan.t.morey@gmail.com>
2018-11-16 01:44:22 -0500

    use array instead of vector at thread

--- problem_1/bucketsort/src/bucketsort.cpp
+++ problem_1/bucketsort/src/bucketsort.cpp
@@ -15,7 +15,8 @@ struct thread_task
     vector<int>& vec;
     int min;
     int max;
-    vector<int>& result;
+    int start;
+    int* sorted;
 };
 
 void print_vec(const vector<int>& vec){
@@ -37,22 +38,18 @@ vector<int> insertion_sort(const vector<int> &unsorted){
                 break;
             }
         }
-        if (!flag){
+        if (!flag)
             sorted.push_back(n);
-        }
     }  
     return sorted;
 }
 
 vector<int> bucketsort(vector<int> &unsorted, int min, int max) {
-    if (unsorted.size() < THRESHOLD){
+    if (unsorted.size() < THRESHOLD)
         return insertion_sort(unsorted);
-    }
 
-    vector<int> sorted;
     vector<int> buckets[N_BUCKETS];
     int bucket_size = (max - min) / N_BUCKETS;
-
     int this_max;
     for (int n : unsorted){
         this_max = min + bucket_size;
@@ -64,9 +61,11 @@ vector<int> bucketsort(vector<int> &unsorted, int min, int max) {
             this_max += bucket_size;
         }
     }
+
     int new_min = min;
     int new_max = min + bucket_size;
     vector<int> sorted_bucket;
+    vector<int> sorted;
     for (int i=0; i<N_BUCKETS-1; i++) {
         sorted_bucket = bucketsort(buckets[i], new_min, new_max); 
         sorted.insert(sorted.end(), sorted_bucket.begin(), sorted_bucket.end());
@@ -79,8 +78,14 @@ vector<int> bucketsort(vector<int> &unsorted, int min, int max) {
 }
 
 void *bucketsort_pthread(void *task){
+    cout << "inside thread";
     thread_task* my_task = (thread_task*)task;
-    task->result = bucketsort(my_task->vec, my_task->min, my_task->max);
+    vector<int> sorted_bucket = bucketsort(my_task->vec, my_task->min, my_task->max);
+    int i = my_task->start;
+    for (auto n: sorted_bucket){
+        my_task->sorted[i] = n;
+        i++;
+    }
     pthread_exit(NULL);
 }
 
@@ -97,35 +102,39 @@ vector<int> bucketsort_threaded(vector<int> &unsorted, int min, int max) {
             this_max += bucket_size;
         }
     }
-
     int new_min = min;
     int new_max = min + bucket_size;
-    vector<int> sorted_buckets[N_THREADS];
+    int sorted[N];
     pthread_t threads[N_THREADS];
+    int start = 0;
     for (int i=0; i<N_THREADS-1; i++) {
         thread_task *task = (thread_task*)malloc(sizeof(thread_task));
         task->min = new_min;
         task->max = new_max;
         task->vec = buckets[i];
-        task->result = vector<int> &sorted_bucket; 
+        task->start = start;
+        task->sorted = sorted;
+        cout << "launching thread";
+        cout << i;
+        cout << '\n';
         pthread_create(&threads[i], NULL, bucketsort_pthread, (void*)task);
+
         new_min = new_max;
         new_max += bucket_size;
+        start += buckets[i].size();
     }
     thread_task *task = (thread_task*)malloc(sizeof(thread_task));
     task->min = new_min;
     task->max = max;
     task->vec = buckets[N_THREADS-1];
-    task->result = vector<int> &sorted_bucket; 
-    pthread_create(&threads[N_THREADS-1], NULL, bucketsort_pthread, (void*)task);
-    for (int i=0;i<N_THREADS;i++){
+    task->start = start;
+    task->sorted = sorted;
+    for (int i=0;i<N_THREADS;i++)
         pthread_join(threads[i], NULL);
-    }
-    vector<int> sorted;
-    for (auto sorted_bucket: sorted_buckets){
-        sorted.insert(sorted.end(), sorted_bucket.begin(), sorted_bucket.end());
-    }
-    return sorted;
+    vector<int> sorted_vec;
+    for (int i=0;i<N;i++)
+        sorted_vec.push_back(sorted[i]);
+    return sorted_vec;
 }
 
 int main(){
@@ -133,9 +142,8 @@ int main(){
     int max = 1000;
     srand(time(nullptr));
     vector<int> unsorted;
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < N; i++)
         unsorted.push_back(min + (rand() % (max - min)));
-    }
     printf("unsorted:\n");
     print_vec(unsorted);
     printf("sequential sorted:\n");
